88d38318300405f7aee57ad5c42e1524a5a4dfa6
Bhavik Bhindora <99002219+BhavikBhindora@users.noreply.github.com>
2023-08-30 14:37:50 +0530

    Update rect_corner.py

--- rect_corner.py
+++ rect_corner.py
@@ -1,3 +1,4 @@
+#Import Library
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -7,11 +8,13 @@ import matplotlib as mpl
 import numpy as np
 import pandas as pd
 
-path = r"D:\Siddharth\segments\Mask_73\10.png"
+#Read Image
+path = r"Path/To/Your/Mask/Image"
 img = cv2.imread(path)
 # mask = np.zeros(img.shape, dtype=np.uint8)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#Find Contours
 contours = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 c = max(contours[0], key = cv2.contourArea)
@@ -19,6 +22,8 @@ x,y,w,h = cv2.boundingRect(c)
 polygon = img.copy()
 a= c.max()
 b = c.min()
+
+#Draw Rectangle and Circle
 img= cv2.rectangle(polygon, (x, y), (x + w, y + h), (0,255,0), 5)
 cv2.circle(polygon,(x+w,y+h),10,(0,0,255),-1)
 cv2.circle(polygon,(x,y),10,(255,0,0),-1) 
@@ -28,7 +33,7 @@ print(x,y,w,h)
 print(a,b)
 
 
-
+#Plot the Fig.
 mpl.rcParams['figure.figsize'] = (12,12); 
 plt.figure()
 plt.subplot(121);plt.imshow(gray);plt.axis('off');plt.title('gray')
